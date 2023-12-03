b54db871a734269c9813cabfa1525180ad3bc2d9
Chirag Bhansali <22539632+chiragbhansali@users.noreply.github.com>
2023-12-03 15:15:42 -0500

    Fix code formatting lexing

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def parse_markdown(program):
     lg.add('BOLD', r'\*\*|\_\_')
     lg.add('ITALIC', r'\*|\_')
     lg.add('STRIKETHROUGH', r'\~\~')
-    lg.add('INLINE_CODE', r'\`')
-    lg.add('CODE_BLOCK', r'\`\`\`')
+    lg.add('CODE_BLOCK', r'(?s)\`\`\`(.*?)\`\`\`')
+    lg.add('INLINE_CODE',  r'(?s)\`(.*?)\`')
     lg.add('LINK_OPEN', r'\[')
     lg.add('LINK_CLOSE', r'\]')
     lg.add('IMAGE', r'\!\[')
@@ -17,7 +17,7 @@ def parse_markdown(program):
     lg.add('BLOCKQUOTE', r'\>')
     lg.add('NEWLINE', r'\n')
     lg.add('WHITESPACE', r'[ \t]+')
-    lg.add('TEXT', r'[^#\*\n\[\]`!\-\~]+')
+    lg.add('TEXT', r'[^#\*\n\[\]`!\-\~\_]+')
 
     lg.ignore(r'[ \t]+')
 
@@ -25,13 +25,16 @@ def parse_markdown(program):
 
     tokens_iter = lexer.lex(program)
 
+    for token in tokens_iter:
+        print(token)
+
     possible_tokens = [rule.name for rule in lexer.rules]
 
     pg = ParserGenerator(possible_tokens,
                            precedence=[
                                ('left', ['BOLD', 'ITALIC']),
                                ('left', ['LINK_OPEN', 'LINK_CLOSE', 'IMAGE', 'URL']),
-                               ('left', ['INLINE_CODE', 'CODE_BLOCK']),
+                               ('left', ['CODE_BLOCK', 'INLINE_CODE']),
                                ('left', ['BLOCKQUOTE', 'LIST_BULLET', 'LIST_NUMBER']),
                                ('left', ['NEWLINE', 'WHITESPACE']),
                             ]
@@ -45,7 +48,6 @@ def parse_markdown(program):
     def elements_many(p):
         return p[0] + p[1]
 
-
     @pg.production('elements : element')
     def elements_single(p):
         return p[0]
@@ -53,7 +55,7 @@ def parse_markdown(program):
     @pg.production('element : HEADING text_content')
     def heading(p):
         level = len(p[0].getstr())
-        return f'<h{level}>{p[1].getstr()}</h{level}>'
+        return f'<h{level}>{p[1]}</h{level}>'
 
     @pg.production('element : text_contents')
     def text_contents_elem(p):
@@ -101,17 +103,17 @@ def parse_markdown(program):
 
     @pg.production('text_content : LINK_OPEN text_content LINK_CLOSE URL')
     def link(p):
-        return f'<a href="{p[3].getstr()[1:-1]}">{p[1].getstr()}</a>'
+        return f'<a href="{p[3].getstr()[1:-1]}">{p[1]}</a>'
 
     @pg.production('element : IMAGE TEXT LINK_CLOSE URL')
     def image(p):
         return f'<img src="{p[3].getstr()[1:-1]}" alt="{p[1].getstr()}">'
 
-    @pg.production('element : CODE_BLOCK TEXT CODE_BLOCK')
+    @pg.production('element : CODE_BLOCK')
     def code_block(p):
         return f'<pre><code>{p[1].getstr()}</code></pre>'
 
-    @pg.production('element : INLINE_CODE TEXT INLINE_CODE')
+    @pg.production('element : INLINE_CODE')
     def inline_code(p):
         return f'<code>{p[1].getstr()}</code>'
 
@@ -132,8 +134,6 @@ def parse_markdown(program):
 
 
 inp = """
-This is ~cut~ text.
-
 Hello
 """
 
@@ -144,7 +144,5 @@ file_path = "output.html"
 # Open the file in write mode and write the HTML content to it
 with open(file_path, "w") as html_file:
     html_file.write(output)
-
-
 
 
